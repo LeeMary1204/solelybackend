925f66eda90a0dbefaa9773272031a2b55888f4a
LeeMary1204 <139114138+LeeMary1204@users.noreply.github.com>
2023-11-19 22:01:56 -0800

    Update scheduleJob.js

--- scheduleJob.js
+++ scheduleJob.js
@@ -81,7 +81,7 @@ const sleep = (n) => {
 }
 
 const scheduleObjectSyntax = () => {
-  schedule.scheduleJob('0 0 0 * * *', () => {
+  schedule.scheduleJob('0 03 22 * * *', () => {
     deleteAll()
     sleep(10000)
 
